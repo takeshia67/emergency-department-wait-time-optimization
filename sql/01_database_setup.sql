1a5b75ddb979530e861e9d4e52bf754dc7967c8a
Takeshia Jones <85200841+takeshia67@users.noreply.github.com>
2026-09-05 21:46:20 -0500

    Create 01_database_setup.sql
    Create MySQL database setup

---
+++ sql/01_database_setup.sql
@@ -0,0 +1,7 @@
+-- Emergency Department Wait-Time Optimization
+-- Project: Healthcare Data Analytics Portfolio
+-- Database: MySQL
+
+CREATE DATABASE emergency_department_analytics;
+
+USE emergency_department_analytics;
